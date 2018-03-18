89cd35f7a6a2c02de217fc6e4cf084a2beddaeec
Velko Ivanov <vivanov@deeperplane.com>
2018-03-18 12:16:18 +0200

    TestTools: ExecutableTester class

--- test/TestTools.h
+++ test/TestTools.h
@@ -20,3 +20,31 @@ TaskOptions GenerateRandomOptions(std::default_random_engine& randEng) {
 
 	return TaskOptions{ priority, isBlocking, target, ms };
 }
+
+class ExecutableTester {
+public:
+	ExecutableTester(std::default_random_engine& _randEng)
+		: randEng(_randEng)
+		, distInt1(1, INT_MAX - 20001)
+		, distInt2(1, 20000)
+	{
+		ResetTest();
+	}
+
+	std::default_random_engine randEng;
+	std::uniform_int_distribution<unsigned int> distInt1;
+	std::uniform_int_distribution<unsigned int> distInt2;
+	unsigned int test;
+	unsigned int testBase;
+	unsigned int generated;
+
+	void PerformTest() {
+		generated = distInt2(randEng);
+		test = testBase + generated;
+	}
+	void ResetTest() {
+		test = 0;
+		testBase = distInt1(randEng);
+		generated = 0;
+	}
+};
