cc194a1d3feefdd6af2bfd981fb4a9895ceccc73
Velko Ivanov <vivanov@deeperplane.com>
2018-03-24 16:56:14 +0200

    Tests: ResourceDeleterTest with gmock

--- test/TestResourcePool.cpp
+++ test/TestResourcePool.cpp
@@ -1,20 +1,67 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 #include <random>
+#include <memory>
 
+#include "Types.h"
 #include "TestTools.h"
+#include "ResourcePool.h"
 
 namespace TasksLib {
 
-	class ResourcePoolTest : public ::testing::Test {
+	template <class T>
+	class MockResourcePool : public ResourcePool<T> {
 	public:
-		ResourcePoolTest::ResourcePoolTest() {
-			randEng.seed(randDev());
+		virtual void Add(std::unique_ptr<T> elem) {
+			Add_(elem.get());
 		}
-		ResourcePoolTest::~ResourcePoolTest() {}
 
-		std::random_device randDev;
-		std::default_random_engine randEng;
+		MOCK_METHOD1_T(Add_, void(T* elem));
+	};
+
+	class ResourceDeleterTest : public TestWithRandom {
+	public:
+		std::shared_ptr<ResourcePool<int> *> resPool;
+		std::weak_ptr<ResourcePool<int> *> resPoolWeak;
+
+		void initPointers(ResourcePool<int> *ptr) {
+			resPool = std::make_shared<ResourcePool<int>*>(ptr);
+			resPoolWeak = std::weak_ptr<ResourcePool<int>*>(resPool);
+		}
+	};
+
+	using namespace ::testing;
+	TEST_F(ResourceDeleterTest, Creates) {
+		initPointers(new ResourcePool<int>{});
+		EXPECT_NO_THROW(ResourceDeleter<int> resDel{ resPoolWeak });
+	}
+	TEST_F(ResourceDeleterTest, WithPoolCallsAdd) {
+		MockResourcePool<int> mockPool;
+
+		initPointers(&mockPool);
+		ResourceDeleter<int> resDel{ resPoolWeak };
+
+		std::unique_ptr<int> ptr(new int{ 174 });
+
+		EXPECT_CALL(mockPool, Add_(ptr.get()));
+		resDel(ptr.release());
+	}
+	TEST_F(ResourceDeleterTest, WithoutPoolDoesntCallAdd) {
+		MockResourcePool<int> mockPool;
+
+		initPointers(&mockPool);
+		ResourceDeleter<int> resDel{ resPoolWeak };
+
+		std::unique_ptr<int> ptr(new int{ 174 });
+		resPool.reset();
+
+		EXPECT_CALL(mockPool, Add_(_)).Times(0);
+		resDel(ptr.release());
+	}
+
+	class ResourcePoolTest : public TestWithRandom {
+	public:
 	};
 
 	TEST_F(ResourcePoolTest, Test1) {
