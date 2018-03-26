ca3093de47cd5ab2e4c06a1db3fe324643344081
Velko Ivanov <vivanov@deeperplane.com>
2018-03-26 17:09:49 +0300

    ResourcePool: Implemented tests, added AcquireAdd(), cosmetics

--- src/ResourcePool.h
+++ src/ResourcePool.h
@@ -15,23 +15,13 @@ namespace TasksLib {
 
 	template <class T>
 	struct ResourceDeleter {
-		explicit ResourceDeleter(std::weak_ptr<ResourcePool<T>*> pool)
-			: pool_(pool) {}
+		ResourceDeleter() = delete;
+		explicit ResourceDeleter(std::weak_ptr<ResourcePool<T>*> pool);
+		ResourceDeleter(const ResourceDeleter<T>& rhs) = delete;
+		ResourceDeleter(const ResourceDeleter<T>&& rhs);
 
-		void operator()(T* ptr) {
-			if (!ptr) {
-				return;
-			}
+		void operator()(T* ptr);
 
-			std::unique_ptr<T> uptr(ptr);
-			if (auto poolPtr = pool_.lock()) {
-				try {
-					(*poolPtr.get())->Add(std::move(uptr));
-					return;
-				}
-				catch (...) {}			// The above is required to never throw by c++ standard
-			}
-		}
 	private:
 		std::weak_ptr<ResourcePool<T>*> pool_;
 	};
@@ -39,40 +29,83 @@ namespace TasksLib {
 	template <class T>
 	class ResourcePool {
 	public:
-		using TPtr = std::unique_ptr<T, ResourceDeleter<T>>;
+		ResourcePool();
+		virtual ~ResourcePool();
 
-		ResourcePool() : thisPtr_(new ResourcePool<T>*{ this }) {}
-		virtual ~ResourcePool() {}
-
-		virtual void Add(std::unique_ptr<T> elem) {
-			std::lock_guard<std::mutex> lock(poolMutex_);
-
-			pool_.push(std::move(elem));
-		}
-
-		TPtr Acquire() {
-			std::lock_guard<std::mutex> lock(poolMutex_);
-
-			T* resourcePtr = nullptr;
-			if (!pool_.empty()) {
-				resourcePtr = pool_.top().release();
-				pool_.pop();
-			}
-
-			TPtr tmpUPtr(resourcePtr, ResourceDeleter{ std::weak_ptr<ResourcePool<T>*>{thisPtr_} });
-			return std::move(tmpUPtr);
-		}
-
-		bool IsEmpty() const {
-			return pool_.empty();
-		}
-		size_t Size() const {
-			return pool_.size();
-		}
+		virtual void Add(std::unique_ptr<T> elem);
+		std::unique_ptr<T, ResourceDeleter<T>> Acquire();
+		std::unique_ptr<T, ResourceDeleter<T>> AddAcquire(std::unique_ptr<T> elem);
+		
+		bool IsEmpty() const;
+		size_t Size() const;
 
 	private:
 		std::mutex poolMutex_;
-		std::shared_ptr<ResourcePool<T>*> thisPtr_;				// This is a shared_ptr to a ptr so that it wouldn't try to double-release *this when destructing the object
+		std::shared_ptr<ResourcePool<T>*> thisPtr_;				// This is a shared_ptr to a ptr so that it wouldn't try to release *this when destructing the object
 		std::stack<std::unique_ptr<T>> pool_;
 	};
+
+	// ==========================================================================
+
+	template <class T>
+	ResourceDeleter<T>::ResourceDeleter(std::weak_ptr<ResourcePool<T>*> pool)
+		: pool_(pool) {}
+	template <class T>
+	ResourceDeleter<T>::ResourceDeleter(const ResourceDeleter<T>&& rhs)
+		: pool_(std::move(rhs.pool_)) {}
+	template <class T>
+	void ResourceDeleter<T>::operator()(T* ptr) {
+		if (!ptr) {
+			return;
+		}
+
+		std::unique_ptr<T> uptr(ptr);
+		if (auto poolPtr = pool_.lock()) {
+			try {
+				(*poolPtr.get())->Add(std::move(uptr));
+				return;
+			}
+			catch (...) {}			// The above is required to never throw by c++ standard
+		}
+	}
+
+	template <class T>
+	ResourcePool<T>::ResourcePool() : thisPtr_(new ResourcePool<T>*{ this }) {}
+	template <class T>
+	ResourcePool<T>::~ResourcePool() {}
+
+	template <class T>
+	void ResourcePool<T>::Add(std::unique_ptr<T> elem) {
+		std::lock_guard<std::mutex> lock(poolMutex_);
+
+		pool_.push(std::move(elem));
+	}
+	template <class T>
+	std::unique_ptr<T, ResourceDeleter<T>> ResourcePool<T>::Acquire() {
+		std::lock_guard<std::mutex> lock(poolMutex_);
+
+		T* resourcePtr = nullptr;
+		if (!pool_.empty()) {
+			resourcePtr = pool_.top().release();
+			pool_.pop();
+		}
+
+		std::unique_ptr<T, ResourceDeleter<T>> tmpUPtr{ resourcePtr, ResourceDeleter<T>{ std::weak_ptr<ResourcePool<T>*>{ thisPtr_ } } };
+		return std::move(tmpUPtr);
+	}
+	template <class T>
+	std::unique_ptr<T, ResourceDeleter<T>> ResourcePool<T>::AddAcquire(std::unique_ptr<T> elem) {
+		T* resourcePtr = elem.release();
+		std::unique_ptr<T, ResourceDeleter<T>> tmpUPtr{ resourcePtr, ResourceDeleter<T>{ std::weak_ptr<ResourcePool<T>*>{ thisPtr_ } } };
+		return std::move(tmpUPtr);
+	}
+
+	template <class T>
+	bool ResourcePool<T>::IsEmpty() const {
+		return pool_.empty();
+	}
+	template <class T>
+	size_t ResourcePool<T>::Size() const {
+		return pool_.size();
+	}
 }
--- test/TestResourcePool.cpp
+++ test/TestResourcePool.cpp
@@ -3,6 +3,7 @@
 
 #include <random>
 #include <memory>
+#include <sstream>
 
 #include "Types.h"
 #include "TestTools.h"
@@ -61,8 +62,58 @@ namespace TasksLib {
 
 	class ResourcePoolTest : public TestWithRandom {
 	public:
+		ResourcePoolTest() {
+			std::uniform_int_distribution<unsigned int> distCount(3, 12);
+			unsigned int i;
+
+			str = GenerateRandomString(15, 25, randEng);
+			Count = distCount(randEng);
+			for (i = 0; i < Count; i++) {
+				pool.Add(std::make_unique<std::string>(str));
+			}
+		}
+
+		std::string str;
+		unsigned int Count;
+		ResourcePool<std::string> pool;
 	};
-	TEST_F(ResourcePoolTest, Test1) {
-		EXPECT_TRUE(true);
+	TEST_F(ResourcePoolTest, Creates) {
+		ResourcePool<int> newPool;
+
+		EXPECT_TRUE(newPool.IsEmpty());
+		EXPECT_EQ(0, newPool.Size());
+	}
+	TEST_F(ResourcePoolTest, Adds) {
+		ASSERT_FALSE(pool.IsEmpty());
+		EXPECT_EQ(pool.Size(), Count);
+
+		std::string str2 = *(pool.Acquire().get());
+		EXPECT_EQ(str2, str);
+	}
+	TEST_F(ResourcePoolTest, AcquiresAndReturns) {
+		ASSERT_FALSE(pool.IsEmpty());
+		EXPECT_EQ(pool.Size(), Count);
+
+		{
+			auto strPtr = pool.Acquire();
+
+			EXPECT_EQ(*strPtr, str);
+			EXPECT_EQ(pool.Size(), Count - 1);
+		}
+
+		EXPECT_EQ(pool.Size(), Count);
+	}
+	TEST_F(ResourcePoolTest, AddAcquire) {
+		ResourcePool<std::string> otherPool;
+		ASSERT_TRUE(otherPool.IsEmpty());
+
+		{
+			auto strPtr = otherPool.AddAcquire(std::make_unique<std::string>(str));
+			EXPECT_EQ(*strPtr, str);
+		}
+		EXPECT_EQ(otherPool.Size(), 1);
+
+		std::string str2 = *(otherPool.Acquire().get());
+		EXPECT_EQ(str, str2);
 	}
 }
